9ef4414335271749dbde1ec77375f0cde1f279e7
oziel10 <rooos.oziel@gmail.com>
2020-10-11 23:58:50 -0500

    Add files via upload

---
+++ build/precache-manifest.28fc2f1809a147a4dad77a302cce24c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09670668200ca9a99b0e4df45bc55ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a93a3db74e3e0f3b987c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5c103c83ead346e44443",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a93a3db74e3e0f3b987c",
+    "url": "/static/js/2.804cb03b.chunk.js"
+  },
+  {
+    "revision": "18db333adb41a97042cb1281e6b75be4",
+    "url": "/static/js/2.804cb03b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c103c83ead346e44443",
+    "url": "/static/js/main.6dd79e70.chunk.js"
+  },
+  {
+    "revision": "f0604a90b7246454fc54",
+    "url": "/static/js/runtime-main.959d7d47.js"
+  }
+]);
---
+++ src/componentes/Datos.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react"
+
+function useDatos() {
+  const [postres, setPostres] = useState([])
+
+  useEffect(() => {
+    fetch("json/postres.json")
+      .then(response => response.json())
+      .then(datos => {
+        setPostres(datos)
+      })
+  }, [])
+
+  return postres
+}
+
+export default function Datos() {
+
+  const postres = useDatos()
+
+  return (
+    <div className="container mt-5" align="center">
+      
+      <h4>Lista de Postres</h4>
+        
+      <div className="row">
+
+        <div className="col-md-12">
+
+          <table className="table table-bordered">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Imagen</th>
+              </tr>
+            </thead> 
+            <tbody>
+
+            {postres.map(item => (
+
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.nombre}</td>
+                <td>{item.stock}</td>
+                <td>{item.precio}</td>
+                <td><img src={`${process.env.PUBLIC_URL}/imagenes/${item.img}`} alt={item.nombre} width="30px" className="img-fluid"/></td>
+              </tr>
+
+            ))}
+
+            </tbody>
+
+          </table>
+
+        </div>
+
+      </div>
+
+     
+        
+    </div>
+
+  )
+}
